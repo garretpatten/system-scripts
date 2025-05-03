77fa0d5f8e51902e02887c1d24e5520bc0cea3d6
Garret Patten <garret.patten@proton.me>
2025-05-02 20:29:00 -0400

    feat: add wsl script

--- scripts/setup-dev-wsl.sh
+++ scripts/setup-dev-wsl.sh
@@ -10,14 +10,14 @@ if [ $? != 0 ]; then
     tmux rename-window 'home'
     tmux send-keys -t $SESSION:0 'home' C-m
 
+    tmux new-window -t $SESSION -n 'os'
+    tmux send-keys -t $SESSION:1 'ollama serve' C-m
+
     tmux new-window -t $SESSION -n 'llm'
     tmux send-keys -t $SESSION:2 'ollama run gemma3' C-m
 
-    tmux new-window -t $SESSION -n 'leetcode'
-    tmux send-keys -t $SESSION:2 'cd $HOME/Projects/leetcode-solutions && lls' C-m
-
     tmux new-window -t $SESSION -n 'projects'
-    tmux send-keys -t $SESSION:3 'cd $HOME/Projects/ && lls' C-m
+    tmux send-keys -t $SESSION:4 'cd $HOME/Projects/ && lls' C-m
 
     tmux set-option -t $SESSION status on
 fi
---
+++ scripts/setup-dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SESSION='dev'
+
+tmux has-session -t $SESSION 2>/dev/null
+
+if [ $? != 0 ]; then
+    tmux new-session -d -s $SESSION
+
+    tmux rename-window 'home'
+    tmux send-keys -t $SESSION:0 'home' C-m
+
+    tmux new-window -t $SESSION -n 'llm'
+    tmux send-keys -t $SESSION:1 'ollama run gemma3' C-m
+
+    tmux new-window -t $SESSION -n 'leetcode'
+    tmux send-keys -t $SESSION:2 'cd $HOME/Projects/leetcode-solutions && lls' C-m
+
+    tmux new-window -t $SESSION -n 'projects'
+    tmux send-keys -t $SESSION:3 'cd $HOME/Projects/ && lls' C-m
+
+    tmux set-option -t $SESSION status on
+fi
+
+tmux select-window -t $SESSION:0
+
+tmux attach-session -t $SESSION
